3de90a898e11dfab2f03ba0136795457ee080dcf
Brando Green <brandeman12@gmail.com>
2024-12-04 16:49:31 -0500

    input actions

--- Assets/PlayerLocomotion.cs
+++ Assets/PlayerLocomotion.cs
@@ -2,17 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLocomotion : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace SG
+{
+    public class PlayerLocomotion : MonoBehaviour
     {
-        
+        Transform cameraObject;
+        InputHandler inputHandler;
+        Vector3 moveDirection;
+
+        [HideInInspector]
+        public Transform myTransform;
+
+        public new Rigidbody rigidbody;
+        public GameObject normalCamera;
+
+        [Header("Stats")]
+        [SerializeField]
+        float movementSpeed = 5;
+        [SerializeField]
+        float rotationSpeed = 10;
+
+        void Start()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+            inputHandler = GetComponent<InputHandler>();
+            cameraObject = Camera.main.transform;
+            myTransform = transform;
+        }
+
+        #region Movement
+        Vector3 normalVector;
+        Vector3 targetPosition;
+
+        private void HandleRotation(float delta)
+        {
+            Vector3 targetDir = Vector3.zero;
+            float moveOverride = inputHandler.moveAmount;
+        }
+
+        #endregion
+
     }
 }
